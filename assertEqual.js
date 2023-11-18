aa2320fde65b1c01ecce6f9f96f0a6b175e20b66
Will Smith <will.w.smith94@gmail.com>
2023-11-18 11:26:46 -0500

    Improved syntax and added more test code to confirm functionality

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,16 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("🛑 Assertion Failed: " + actual + " !== " + expected);
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual(1, "One");
+assertEqual("Chew", "Chew")
+assertEqual("1", 1); // Just for fun :)
