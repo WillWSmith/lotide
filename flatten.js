4098fa574447a4727fc2591c263d1349db829494
Will Smith <will.w.smith94@gmail.com>
2023-11-25 08:12:10 -0500

    Edited flatten function based on AI feedback. Properly checks for nested array

--- flatten.js
+++ flatten.js
@@ -24,7 +24,11 @@ function flatten(array) {
   let result = [];
 
   for (let i = 0; i < array.length; i++) {
-    result = result.concat(array[i]);
+    if (Array.isArray(array[i])) {
+      result = result.concat(array[i]);
+    } else {
+      result.push(array[i]);
+    }
   }
 
   return result;
