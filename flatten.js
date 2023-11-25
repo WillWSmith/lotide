0d54548f7649577b68cd71430411011abf33ca88
Will Smith <will.w.smith94@gmail.com>
2023-11-25 08:05:19 -0500

    Created a function flatten which will take in an array containing elements including nested arrays of elements, and return a flattened version of the array.

---
+++ flatten.js
@@ -0,0 +1,51 @@
+function eqArrays(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Arrays are equal: ${array1} === ${array2}`); 
+  } else {
+    console.log(`❌ Arrays are NOT equal: ${array1} !== ${array2}`); 
+  }
+};
+
+function flatten(array) {
+  let result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    result = result.concat(array[i]);
+  }
+
+  return result;
+}
+
+// Test case 1: Basic test with positive numbers
+const nestedArray1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+const result1 = flatten(nestedArray1);
+assertArraysEqual(result1, [1, 2, 3, 4, 5, 6, 7, 8, 9]); // ✅ 
+
+// Test case 2: Basic test with mixed types
+const nestedArray2 = [[1, "two", 3], ["four", 5, "six"], [7, "eight", 9]];
+const result2 = flatten(nestedArray2);
+assertArraysEqual(result2, [1, "two", 3, "four", 5, "six", 7, "eight", 9]); // ✅ 
+
+// Test case 3: Flattening an empty nested array
+const nestedArray3 = [];
+const result3 = flatten(nestedArray3);
+assertArraysEqual(result3, []); // ✅ 
+
+// Test case 4: Test where flattening might fail (undefined variable)
+const nestedArray4 = [[1, 2, 3], [4, 5, 6], [7, 8, "9"]]; //String "9" instead of element 9
+const result4 = flatten(nestedArray4);
+assertArraysEqual(result4, []); // ❌
