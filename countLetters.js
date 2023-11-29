5870ae11db945602299a8a9475f60e102b5cc08c
Will Smith <will.w.smith94@gmail.com>
2023-11-29 12:47:54 -0500

    Removed the symbol filter as it was not part of project as AI pointed out, also cleaned up an extra console.log from debugging.

--- countLetters.js
+++ countLetters.js
@@ -7,17 +7,13 @@ const assertEqual = function(actual, expected) {
 };
 
 countLetters = function(string) {
-  const cleanString = string.replace(/[^a-zA-Z]/g, ''); //remove non letters
-  
   const count = {}; //object to store letter count
 
-  for (const char of cleanString) {  //loops through each character of the clean string
+  for (const char of string) {  //loops through each character of the clean string
     count[char] = (count[char] || 0) + 1; //keeps track of how many of each letter
   }
   return count; //returns the results
 };
-
-console.log(countLetters(`LHL`));
 
 const result1 = countLetters("LHL");
 assertEqual(result1.L, 2);
