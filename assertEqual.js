88d81a9b7fbea0138120661e7f0ec89a6a6ddbca
Will Smith <will.w.smith94@gmail.com>
2023-12-08 08:28:17 -0500

    Refactored assertEqual and head to be modularized

--- assertEqual.js
+++ assertEqual.js
@@ -7,10 +7,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual(1, "One");
-assertEqual("Chew", "Chew")
-assertEqual("1", 1); // Just for fun :)
+module.exports = assertEqual;
---
+++ test/assertEqualTest.js
@@ -0,0 +1,9 @@
+const assertEqual = require(`../assertEqual`);
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual(1, "One");
+assertEqual("Chew", "Chew")
+assertEqual("1", "1");
